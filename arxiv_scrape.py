5e8cf0f1e4a83fbae73cf0ad765a81e1f2ecdd80
Bilal AbdulRahman <53377124+bilalze@users.noreply.github.com>
2023-12-04 17:05:28 -0500

    Update arxiv_scrape.py

--- arxiv_scrape.py
+++ arxiv_scrape.py
@@ -1,16 +1,28 @@
 import arxiv
 #create search query
-search = arxiv.Search(
-  query = "computer vision machine learning",
-  max_results = 10,
-  sort_by = arxiv.SortCriterion.SubmittedDate
-)
-#get results
-for result in arxiv.Client().results(search):
-  print(result.title)
-  print(result.pdf_url)
+# class paper_search:
+#   def __init__(self, max_result=10,sort_by=arxiv.SortCriterion.SubmittedDate):
+#     self.max_result = max_result
+#     self.sort_by=sort_by
+
+def search_arxiv(query, max_result=10, sort_by=arxiv.SortCriterion.SubmittedDate):
+    search = arxiv.Search(query=query, max_results=max_result, sort_by=sort_by)
+    return search
+
+def get_arxiv_results(search, download=False):
+    papers_title = []
+    papers_url = []
+    
+    for result in arxiv.Client().results(search):
+        papers_title.append(result.title)
+        papers_url.append(result.pdf_url)
+        
+        if download:
+            result.download_pdf(filename=f"{result.title}.pdf")
+    
+    return papers_title, papers_url
 
 #download pdf example will download the first result
-paper = next(arxiv.Client().results(search))
-paper.download_pdf(filename="downloaded-paper.pdf")
+# paper = next(arxiv.Client().results(search))
+# paper.download_pdf(filename="downloaded-paper.pdf")
 
